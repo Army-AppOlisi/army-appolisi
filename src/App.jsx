a558cb816a7c935a388e0ac34718680fb8fd143b
Evmorfia Elpida Dasyra <dasyevita@gmail.com>
2024-02-21 18:50:05 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>HELLO</h2>
+      <p>YOU ARE WELCOME</p>
+    </>
+  )
+}
+
+export default App
