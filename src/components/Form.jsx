175bf53a73ded32daaacaf2212b095c447b46432
dasyevita <dasyevita@gmail.com>
2024-03-04 13:12:05 +0200

    handle form

---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+import DateInput from "./DateInput";
+import MonthsInput from "./MonthsInput";
+
+import { Card, Button, Typography } from "@material-tailwind/react";
+
+export default function Form() {
+  return (
+    <Card className="flex items-center" color="transparent" shadow={false}>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <div className="mb-1 flex flex-col gap-6">
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Ποια είναι η πρώτη μέρα της θητείας σου;
+          </Typography>
+          <DateInput size="lg" />
+          {/* <Input
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          /> */}
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Πόσους μήνες θα υπηρετήσεις;
+          </Typography>
+          <MonthsInput />
+        </div>
+        {/* <Button className="mt-6" fullWidth>
+          Results
+        </Button> */}
+      </form>
+    </Card>
+  );
+}
