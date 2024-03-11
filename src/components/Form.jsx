a4298c4fdbf5db2cd3cd140450b9ff17b369dd63
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2024-03-11 14:39:58 +0200

    feat: show results after submit form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,28 +1,40 @@
 import DateInput from "./DateInput";
 import MonthsInput from "./MonthsInput";
 import Results from "./Results";
+import { useState } from "react";
 
 import { Card, Button, Typography } from "@material-tailwind/react";
 
 export default function Form() {
+
+
+  const [showResults, setShowResults] = useState(false);
+
+  // Function to handle displaying the results
+  const handleShowResults = () => {
+    setShowResults(true);
+  }
   return (
     <Card className="flex items-center" color="transparent" shadow={false}>
-      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-        <div className="mb-1 flex flex-col gap-6">
-          <Typography variant="h6" color="blue-gray" className="-mb-3">
-            Ποια είναι η πρώτη μέρα της θητείας σου;
-          </Typography>
-          <DateInput size="lg" />
-          <Typography variant="h6" color="blue-gray" className="-mb-3">
-            Πόσους μήνες θα υπηρετήσεις;
-          </Typography>
-          <MonthsInput />
-        </div>
-        <Button className="mt-6" fullWidth>
-          Results
-        </Button>
-      </form>
-      <Results /> 
+      {showResults ? (
+        <Results />
+      ) : (
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Ποια είναι η πρώτη μέρα της θητείας σου;
+            </Typography>
+            <DateInput size="lg" />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Πόσους μήνες θα υπηρετήσεις;
+            </Typography>
+            <MonthsInput />
+          </div>
+          <Button className="mt-6" fullWidth onClick={handleShowResults}>
+            Results
+          </Button>
+        </form>
+      )}
     </Card>
   );
 }
